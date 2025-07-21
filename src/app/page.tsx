d01611011f9490962a4531b301a429465859ecd1
yuki-o-tech <yuki.otsubo.tech@gmail.com>
2025-07-21 18:33:16 +0900

    refactor: remove semi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import Image from "next/image"
 
-export default function Home() {
+export default function Start() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -99,5 +99,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
