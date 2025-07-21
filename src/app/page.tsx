a8250f097108cc722eb45af953d4c911e160cfac
yuki-o-tech <yuki.otsubo.tech@gmail.com>
2025-07-21 16:40:38 +0900

    Add: Next.js, TypeScript, TailwindCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Hi! Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
